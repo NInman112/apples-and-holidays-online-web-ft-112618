669fc0bc015e9dade0356fbc8e9140cdb110ed4b
Nick Inman <nick@theoatmeal.com>
2018-11-27 20:52:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
   holiday_hash.each do |season, data|
     puts "#{season.capitalize}:"
     data.each do |holiday, supplies|
